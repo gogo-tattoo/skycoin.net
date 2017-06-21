96dbb6bb1a6984eedf0b7916dc4c6aadb6d360f4
Angus Fretwell <angusfretwell@me.com>
2017-06-21 23:04:11 +1000

    refactor: convert css to styled components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import React, { Component } from 'react';
+import styled, { keyframes } from "styled-components"
 import logo from './logo.svg';
-import './App.css';
+
+const spin = keyframes`
+  from { transform: rotate(0deg); }
+  to { transform: rotate(360deg); }
+`;
+
+const Container = styled.div`
+  text-align: center;
+`;
+
+const Logo = styled.img`
+  animation: ${spin} infinite 20s linear;
+  height: 80px;
+`;
+
+const Header = styled.div`
+  background-color: #222;
+  height: 150px;
+  padding: 20px;
+  color: white;
+`;
+
+const Intro = styled.p`
+  font-size: large;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <Container>
+        <Header>
+          <Logo src={logo} alt="logo" />
           <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
+        </Header>
+        <Intro>
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        </Intro>
+      </Container>
     );
   }
 }
