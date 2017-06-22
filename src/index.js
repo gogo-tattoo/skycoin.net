cda4af22fd8f7f069b034e3328d87c93cce79e64
Angus Fretwell <angusfretwell@me.com>
2017-06-22 23:48:35 +1000

    feat: add normalize.css and css reset

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { render } from 'react-snapshot';
+
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+
+import 'normalize.css/normalize.css';
 import './index.css';
 
 render(<App />, document.getElementById('root'));
