8f7f6e19f72dcd7794178e407e6e4103e10976fb
Angus Fretwell <angusfretwell@me.com>
2017-06-23 13:52:17 +1000

    feat: basic button styles

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,6 +1,11 @@
-import React from 'react';
 import styled from 'styled-components';
 
 export default styled.button`
-
+  appearance: none;
+  background: none;
+  border-radius: 1000px;
+  border: 1px solid;
+  padding: 0.75em 1.5em;
+  line-height: 1em;
+  cursor: pointer;
 `;
