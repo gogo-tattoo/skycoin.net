dcd8e5cf584b0bb67a8a89e68b86566f2495a12d
Angus Fretwell <angusfretwell@me.com>
2017-06-23 13:52:05 +1000

    feat: add logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-export default () => (
+import logo from './logo.svg';
+
+const Logo = styled.img.attrs({
+  src: logo,
+  alt: 'Skycoin',
+})`
+  height: 2.125rem;
+`;
+
+export default (props) => (
   <Link to="/">
-    <img src="" alt="Skycoin" />
+    <Logo {...props} />
   </Link>
 );
