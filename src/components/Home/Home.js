d7f013c1383ed6be359f06b6b6ae184a343fc1d8
Angus Fretwell <angusfretwell@me.com>
2017-06-23 14:37:12 +1000

    feat: add padding and background to header

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Header from '../Header';
 
+const Home = styled.div`
+  background-color: #4990e2;
+  padding: 1rem 0;
+`;
+
 export default () => (
-  <Header />
+  <Home>
+    <Header />
+  </Home>
 );
