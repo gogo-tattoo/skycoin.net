1b7f94627f9388bbceee812a6e2e6d49ae0312a8
Angus Fretwell <angusfretwell@me.com>
2017-07-02 15:20:35 +1000

    feat: add hover and active states to button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,11 @@
 import styled, { css } from 'styled-components';
 import { space, width, fontSize, color } from 'styled-system';
-import { rem } from 'polished';
+import { rem, darken } from 'polished';
+import get from 'lodash/get';
 
 import Link from 'components/Link';
 import media from 'utils/media';
-import { FONT_FAMILIES, FONT_SIZES, SPACE, BORDER_RADIUS, BOX_SHADOWS } from 'config';
+import { COLORS, FONT_FAMILIES, FONT_SIZES, SPACE, BORDER_RADIUS, BOX_SHADOWS } from 'config';
 
 export default styled(Link)`
   display: inline-block;
@@ -20,15 +21,41 @@ export default styled(Link)`
   font-family: ${FONT_FAMILIES.mono};
   font-size: ${rem(FONT_SIZES[3])};
   padding: ${rem(SPACE[3])} ${rem(SPACE[6])};
+  transition: 150ms ease-in-out;
 
   ${fontSize}
   ${color}
   ${space}
   ${width}
 
+  &:hover {
+    background-color: ${props =>
+      props.bg && darken(0.05, get(COLORS, props.bg) || props.bg)
+    };
+
+    box-shadow: ${BOX_SHADOWS.hover};
+  }
+
+  &:active {
+    background-color: ${props =>
+      props.bg && darken(0.1, get(COLORS, props.bg) || props.bg)
+    };
+  }
+
   ${props => props.outlined && css`
     border: 1px solid currentColor;
-    box-shadow: none;
+
+    &, &:hover, &:active {
+      box-shadow: none;
+    }
+
+    &:hover {
+      background-color: rgba(0, 0, 0, 0.05);
+    }
+
+    &:active {
+      background-color: rgba(0, 0, 0, 0.1);
+    }
   `}
 
   ${props => props.big && media.sm.css`
--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 import palx from 'palx';
 
+export const COUNTDOWN_TARGET = new Date('2017-08-01T04:00:00.000Z');
 export const COLORS = palx('#4990e2');
 export const SPACE = [0, 4, 8, 12, 16, 20, 24, 32, 40, 48, 56, 64, 72];
 export const FONT_SIZES = [11, 13, 14, 15, 17, 20, 24, 28, 36, 40];
@@ -22,4 +23,5 @@ export const BORDER_RADIUS = {
 
 export const BOX_SHADOWS = {
   base: '0 1px 2px rgba(0, 0, 0, 0.25)',
+  hover: '0 1px 4px rgba(0, 0, 0, 0.25)',
 };
