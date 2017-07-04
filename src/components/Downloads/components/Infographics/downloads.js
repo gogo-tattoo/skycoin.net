5b19f2ac81e9d361360109bc5242454610c4a2f1
Angus Fretwell <angusfretwell@me.com>
2017-07-04 21:31:38 +1000

    chore: add infographic download links

--- src/components/Downloads/components/Infographics/downloads.js
+++ src/components/Downloads/components/Infographics/downloads.js
@@ -1,6 +1,6 @@
 export default [{
   name: 'Skycoin: How it Works',
-  download: '',
+  download: 'http://downloads.skycoin.net/infographics/skycoin-how-it-works.zip',
   filetype: '.zip',
-  filesize: '100kb',
+  filesize: '764kb',
 }];
