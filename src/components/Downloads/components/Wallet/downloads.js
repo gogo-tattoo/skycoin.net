9677277e8924c55716e9836bf6ca59bc3f12ae06
iketheadore <heaven.mao@gmail.com>
2017-07-13 19:45:32 +0800

    Update download for 0.19.0 (#15)

--- src/components/Downloads/components/Wallet/downloads.js
+++ src/components/Downloads/components/Wallet/downloads.js
@@ -1,4 +1,4 @@
-export const version = 'v0.18.0';
+export const version = 'v0.19.0';
 export const downloads = [{
   platform: 'downloads.wallet.macos',
   icon: 'macos',
@@ -6,19 +6,19 @@ export const downloads = [{
     name: 'downloads.wallet.withElectron',
     architectures: [{
       name: '',
-      download: 'http://downloads.skycoin.net/wallet/skycoin-0.18.0-gui-osx-x64.dmg',
-      torrent: 'http://downloads.skycoin.net/wallet/skycoin-0.18.0.torrent',
+      download: 'http://downloads.skycoin.net/wallet/skycoin-0.19.0-gui-osx-x64.dmg',
+      torrent: 'http://downloads.skycoin.net/wallet/skycoin-0.19.0.torrent',
       filetype: '.dmg',
-      filesize: '45.8mb',
+      filesize: '44mb',
     }],
   }, {
     name: 'downloads.wallet.withoutElectron',
     architectures: [{
       name: '',
-      download: 'http://downloads.skycoin.net/wallet/skycoin-0.18.0-bin-osx-darwin-x64.zip',
-      torrent: 'http://downloads.skycoin.net/wallet/skycoin-0.18.0.torrent',
+      download: 'http://downloads.skycoin.net/wallet/skycoin-0.19.0-bin-osx-darwin-x64.zip',
+      torrent: 'http://downloads.skycoin.net/wallet/skycoin-0.19.0.torrent',
       filetype: '.zip',
-      filesize: '11.1mb',
+      filesize: '11mb',
     }],
   }],
 }, {
@@ -28,23 +28,23 @@ export const downloads = [{
     name: 'downloads.wallet.withElectron',
     architectures: [{
       name: '64bit',
-      download: 'http://downloads.skycoin.net/wallet/skycoin-0.18.0-gui-win-setup.exe',
-      torrent: 'http://downloads.skycoin.net/wallet/skycoin-0.18.0.torrent',
+      download: 'http://downloads.skycoin.net/wallet/skycoin-0.19.0-gui-win-setup.exe',
+      torrent: 'http://downloads.skycoin.net/wallet/skycoin-0.19.0.torrent',
       filetype: '.exe',
-      filesize: '70.5mb',
+      filesize: '68mb',
     }],
   }, {
     name: 'downloads.wallet.withoutElectron',
     architectures: [{
       name: '32bit',
-      download: 'http://downloads.skycoin.net/wallet/skycoin-0.18.0-bin-win-x86.zip',
-      torrent: 'http://downloads.skycoin.net/wallet/skycoin-0.18.0.torrent',
+      download: 'http://downloads.skycoin.net/wallet/skycoin-0.19.0-bin-win-x86.zip',
+      torrent: 'http://downloads.skycoin.net/wallet/skycoin-0.19.0.torrent',
       filetype: '.zip',
-      filesize: '10.8mb',
+      filesize: '11mb',
     }, {
       name: '64bit',
-      download: 'http://downloads.skycoin.net/wallet/skycoin-0.18.0-bin-win-x64.zip',
-      torrent: 'http://downloads.skycoin.net/wallet/skycoin-0.18.0.torrent',
+      download: 'http://downloads.skycoin.net/wallet/skycoin-0.19.0-bin-win-x64.zip',
+      torrent: 'http://downloads.skycoin.net/wallet/skycoin-0.19.0.torrent',
       filetype: '.zip',
       filesize: '11mb',
     }],
@@ -56,25 +56,25 @@ export const downloads = [{
     name: 'downloads.wallet.withElectron',
     architectures: [{
       name: '64bit',
-      download: 'http://downloads.skycoin.net/wallet/skycoin-0.18.0-gui-linux-x64.AppImage',
-      torrent: 'http://downloads.skycoin.net/wallet/skycoin-0.18.0.torrent',
+      download: 'http://downloads.skycoin.net/wallet/skycoin-0.19.0-gui-linux-x64.AppImage',
+      torrent: 'http://downloads.skycoin.net/wallet/skycoin-0.19.0.torrent',
       filetype: '.AppImage',
-      filesize: '61mb',
+      filesize: '59mb',
     }],
   }, {
     name: 'downloads.wallet.withoutElectron',
     architectures: [{
       name: '32bit',
-      download: 'http://downloads.skycoin.net/wallet/skycoin-0.18.0-bin-linux-arm.tar.gz',
-      torrent: 'http://downloads.skycoin.net/wallet/skycoin-0.18.0.torrent',
+      download: 'http://downloads.skycoin.net/wallet/skycoin-0.19.0-bin-linux-arm.tar.gz',
+      torrent: 'http://downloads.skycoin.net/wallet/skycoin-0.19.0.torrent',
       filetype: '.tar.gz',
-      filesize: '8.2mb',
+      filesize: '7.9mb',
     }, {
       name: '64bit',
-      download: 'http://downloads.skycoin.net/wallet/skycoin-0.18.0-bin-linux-x64.tar.gz',
-      torrent: 'http://downloads.skycoin.net/wallet/skycoin-0.18.0.torrent',
+      download: 'http://downloads.skycoin.net/wallet/skycoin-0.19.0-bin-linux-x64.tar.gz',
+      torrent: 'http://downloads.skycoin.net/wallet/skycoin-0.19.0.torrent',
       filetype: '.tar.gz',
-      filesize: '8.4mb',
+      filesize: '8.1mb',
     }],
   }],
 }];
