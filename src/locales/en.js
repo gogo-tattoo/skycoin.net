e3a7b6cae2b86887415bbb518e03328e9646db32
Angus Fretwell <angusfretwell@me.com>
2017-07-16 20:20:24 +1000

    refactor: change distribution date to noon gmt

--- src/locales/en.js
+++ src/locales/en.js
@@ -137,7 +137,7 @@ export default {
       body: 'Distribution will be available through the skycoin.net website or through a <a href="https://t.me/Skycoin" target="_blank">telegram bot.</a>',
     },
     countdown: {
-      heading: 'The distribution event will begin on <strong>July 24</strong> at midnight GMT.',
+      heading: 'The distribution event will begin on <strong>July 24</strong> at noon GMT.',
       days: 'Days',
       hours: 'Hours',
       minutes: 'Minutes',
