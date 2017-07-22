ab5c9c5d29504a28287e7708635fc36e82b04994
Angus Fretwell <angusfretwell@me.com>
2017-07-23 01:51:54 +1000

    fix: add date value to distribution meta description

--- src/components/Distribution/Distribution.js
+++ src/components/Distribution/Distribution.js
@@ -2,7 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import { injectIntl } from 'react-intl';
+import moment from 'moment';
 
+import { DISTRIBUTION_START } from 'config';
 import GetStarted from 'components/GetStarted';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
@@ -18,7 +20,11 @@ const Distribution = ({ intl }) => (
       <title>{intl.formatMessage({ id: 'distribution.title' })}</title>
       <meta
         name="description"
-        content={intl.formatMessage({ id: 'distribution.description' })}
+        content={intl.formatMessage({
+          id: 'distribution.description',
+        }, {
+          date: moment(DISTRIBUTION_START).format('LL'),
+        })}
       />
     </Helmet>
     <Header />
