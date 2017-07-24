40b3e22fff71aef35becef94b2c99b922928e6af
Angus Fretwell <angusfretwell@me.com>
2017-07-24 15:21:48 +1000

    fix: missing date value

--- src/components/Home/components/Hero/components/Announcement/Announcement.js
+++ src/components/Home/components/Hero/components/Announcement/Announcement.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Flex, Box } from 'grid-styled';
 import { rem } from 'polished';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, injectIntl } from 'react-intl';
+import moment from 'moment';
 
+import { DISTRIBUTION_START } from 'config';
 import Link from 'components/Link';
 import Container from 'components/Container';
 import Text from 'components/Text';
 import megaphone from './megaphone.png';
 
-const Announcement = styled.div`
+const Wrapper = styled.div`
   background-color: rgba(0, 0, 0, 0.25);
   width: 100%;
 `;
@@ -24,8 +27,8 @@ const Icon = styled.img.attrs({
   width: ${rem(32)};
 `;
 
-export default () => (
-  <Announcement>
+const Announcement = ({ intl }) => (
+  <Wrapper>
     <Container>
       <StyledLink to="distribution">
         <Flex align="center">
@@ -35,11 +38,24 @@ export default () => (
 
           <Box>
             <Text color="white" fontSize={[1, 3]} heavy py={4} m={0}>
-              <FormattedMessage id="home.hero.announcement" />
+              <FormattedMessage
+                id="home.hero.announcement"
+                values={{
+                  date: moment(DISTRIBUTION_START).locale(intl.locale).format('LL'),
+                }}
+              />
             </Text>
           </Box>
         </Flex>
       </StyledLink>
     </Container>
-  </Announcement>
+  </Wrapper>
 );
+
+Announcement.propTypes = {
+  intl: PropTypes.shape({
+    locale: PropTypes.string,
+  }).isRequired,
+};
+
+export default injectIntl(Announcement);
