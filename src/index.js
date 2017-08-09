284da5692a1079e1afbd6ec60bbe60af5a363668
Angus Fretwell <angusfretwell@me.com>
2017-08-09 18:46:25 +1000

    refactor: remove service worker (#37)

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import 'normalize.css/normalize.css';
 import './index.css';
 
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 
 render(<App />, document.getElementById('root'));
-registerServiceWorker();
+unregister();
